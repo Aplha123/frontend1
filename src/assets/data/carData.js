4f479fcb9e42525fe865964eca3d80e49f0af32f
chetanya singh bais <94671552+Aplha123@users.noreply.github.com>
2023-03-28 19:31:09 +0530

    Add files via upload

---
+++ src/assets/data/carData.js
@@ -0,0 +1,142 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/nissan-offer.png";
+import img02 from "../all-images/cars-img/mercedes-offer.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+import img04 from "../all-images/cars-img/harvester.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/harvestercumharvester.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Harvester",
+    rating: 112,
+    carName: "Harvester",
+    imgUrl: img01,
+    model: "Model 3",
+    price: 1000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "",
+    rating: 102,
+    carName: "Buldozers",
+    imgUrl: img02,
+    model: "Model-2022",
+    price: 1500,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "",
+    rating: 132,
+    carName: "Tractors",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: 5000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Harvester",
+    rating: 102,
+    carName: "Harvester with truck",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 70,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Ferrari",
+    rating: 94,
+    carName: "Ferrari Camry",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: 45,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Mercedes",
+    rating: 119,
+    carName: "Triller",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: 85,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  // {
+  //   id: 7,
+  //   brand: "Audi",
+  //   rating: 82,
+  //   carName: "Audi Fiesta",
+  //   imgUrl: img07,
+  //   model: "Model 3",
+  //   price: 50,
+  //   speed: "20kmpl",
+  //   gps: "GPS Navigation",
+  //   seatType: "Heated seats",
+  //   automatic: "Automatic",
+  //   description:
+  //     " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  // },
+
+  // {
+  //   id: 8,
+  //   brand: "Colorado",
+  //   rating: 52,
+  //   carName: "Rolls Royce Colorado",
+  //   imgUrl: img08,
+  //   model: "Model 3",
+  //   price: 50,
+  //   speed: "20kmpl",
+  //   gps: "GPS Navigation",
+  //   seatType: "Heated seats",
+  //   automatic: "Automatic",
+  //   description:
+  //     " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  // },
+];
+
+export default carData;
+
+
+// ../all-images/cars-img/nissan-offer.png
